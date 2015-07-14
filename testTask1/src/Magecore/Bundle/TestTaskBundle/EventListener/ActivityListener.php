23440a2f6ceda17d483530f1f9114ab527a8af9d
JudeForOROINC <gkudryashov@magecore.com>
2015-07-14 09:08:37 +0300

    OOT-777: made a unit test fix and code style fix  - refactor. rfix codestyle.

--- testTask1/src/Magecore/Bundle/TestTaskBundle/EventListener/ActivityListener.php
+++ testTask1/src/Magecore/Bundle/TestTaskBundle/EventListener/ActivityListener.php
@@ -18,126 +18,43 @@ use DateTime;
 use Magecore\Bundle\TestTaskBundle\Entity\User;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class ActivityListener{
+class ActivityListener
+{
 
     protected $notifer=array();
 
     protected $container=null;
 
-    protected function push_mail(Activity $activity){
+    /**
+     * @param Activity $activity
+     */
+    protected function pushMail(Activity $activity)
+    {
         $mailer = $this->container->get('my.mailer');
         $mailer->PushMail($activity);
     }
 
-    protected function pushmail(Activity $activity){
-
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Hello Email')
-            ->setFrom('send@example.com')
-            ->setTo('recipient@example.com')
-            ->setBody(
-                $this->renderView(
-                // app/Resources/views/Emails/registration.html.twig
-                    'Emails/registration.html.twig',
-                    array('name' => $name)
-                ),
-                'text/html'
-            )
-            /*
-             * If you also want to include a plaintext version of the message
-            ->addPart(
-                $this->renderView(
-                    'Emails/registration.txt.twig',
-                    array('name' => $name)
-                ),
-                'text/plain'
-            )
-            */
-        ;
-        $this->get('mailer')->send($message);
-
-
-
-        $trans = \Swift_SmtpTransport::newInstance(
-            '127.0.0.1',1025
-        );
-            $maler = \Swift_Mailer::newInstance($trans);
-        /*
-        $cntr = New MaillerController($this->container);
-*/
-        $cntr = $this->container->get('my.mailer');
-
-        $arr = $cntr->FormMailAction($activity);
-
-        if (!empty($arr) && count($arr )){
-
-            foreach($arr as $letter){
-                $message = \Swift_Message::newInstance()
-                    ->setSubject($letter['title'])
-                    ->setFrom('send@example.com')
-                    ->setTo($letter['mail'])
-                    ->setBody(
-                        $letter['letter'],'text/html');
-
-//
-                $result = $maler->send($message);
-
-        }
-            }
-
-//
-//        $message = \Swift_Message::newInstance()
-//            ->setSubject('Hello Email')
-//            ->setFrom('send@example.com')
-//            ->setTo('recipient@example.com')
-//            ->setBody(
-////                $this->renderView(
-////                // app/Resources/views/Emails/registration.html.twig
-////                    'Emails/registration.html.twig',
-////                    array('name' => $name)
-////                ),
-//                '<body>mama mila ramu</body>',
-//                'text/html'
-//            )
-//            /*
-//             * If you also want to include a plaintext version of the message
-//            ->addPart(
-//                $this->renderView(
-//                    'Emails/registration.txt.twig',
-//                    array('name' => $name)
-//                ),
-//                'text/plain'
-//            )
-//            */
-        ;
-    //   $result = $maler->send($message);
-
-    }
-    //
 
     /**
      * @param LifecycleEventArgs $args
      * we add activity when new issue was created;
      */
-    public function postPersist(LifecycleEventArgs $args){
+    public function postPersist(LifecycleEventArgs $args)
+    {
         $entity = $args->getEntity();
         $entityManager = $args->getEntityManager();
 
         // new entity inserted in database. may be a Issue.
         if ($entity instanceof Issue) {
-
             $entity->setCode($entity->getProject()->getCode().'-'.$entity->getId());
-
             $active = new Activity();
             $active->setType($active::ACTIVITY_TYPE_CREATE_ISSUE);
             $active->setIssue($entity);
-            $active->setUser($entity->getReporter());//Here is dangerous: Reporter is not User - if we change logic. but until we controlling code we hope it is.
-            //TODO Fix this bug!!! must get a real user.
-            $entityManager->persist( $active);
-            //$entityManager->persist( $entity);//?/?
+            $active->setUser($entity->getReporter());
+            //Here is dangerous: Reporter is not User - if we change logic. but until we controlling code we hope it is.
+            $entityManager->persist($active);
             $entityManager->flush();
 
-            //$this->pushmail($active);
         }
 
         // new entity inserted in database. may be a Comment.
@@ -145,22 +62,19 @@ class ActivityListener{
             $active = new Activity();
             $active->setType($active::ACTIVITY_TYPE_COMMENT_IN_ISSUE);
             $active->setIssue($entity->getIssue());
-            $active->setUser($entity->getAuthor());//Here is dangerous: Reporter is not User - if we change logic. but until we controlling code we hope it is.
+            $active->setUser($entity->getAuthor());
+            //Here is dangerous: Author is not User - if we change logic. but until we controlling code we hope it is.
             $active->setComment($entity);
-            $entityManager->persist( $active);
+            $entityManager->persist($active);
             $entityManager->flush();
-
-            //$this->pushmail($active);
-
         }
         if ($entity instanceof Activity) {
-            //$this->pushmail($entity);
-            $this->push_mail($entity);
-            //push_mail
+            $this->pushMail($entity);
         }
     }
 
-    public function preUpdate(PreUpdateEventArgs $args){
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
         $entity = $args->getEntity();
         $entityManager = $args->getEntityManager();
         $ch = $args->getEntityChangeSet();
@@ -175,7 +89,7 @@ class ActivityListener{
 
                 $currentUser = $this->container->get('security.context')->getToken()->getUser();
 
-                $active->setUser($currentUser);//Here is dangerous: Reporter is not User - if we change logic. but until we controlling code we hope it is.
+                $active->setUser($currentUser);
                 $active->setFromIssueStatus($args->getOldValue('status'));
                 $active->setToIssueStatus($args->getNewValue('status'));
                 $this->notifer[]=$active;
@@ -183,17 +97,19 @@ class ActivityListener{
         }
     }
 
-    public function postUpdate(LifecycleEventArgs $args){//TODO use postflush
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        //TODO use postflush
         $entity = $args->getEntity();
         $entityManager = $args->getEntityManager();
 
         // if event was enabled - try to insert log record;
         if ($entity instanceof Issue) {
             // event list is not empty
-            if ( count ($this->notifer) ) {
+            if (count($this->notifer)) {
                 $arr = $this->notifer;
                 $this->notifer = array();
-                foreach($arr as $line){
+                foreach ($arr as $line) {
                     $entityManager->persist($line);
                 }
                 $entityManager->flush();
@@ -213,7 +129,8 @@ class ActivityListener{
         $entities = array_merge($unitOfWork->getScheduledEntityInsertions(), $unitOfWork->getScheduledEntityUpdates());
         foreach ($entities as $entity) {
             $reflect = new \ReflectionObject($entity);
-            foreach ($reflect->getProperties(\ReflectionProperty::IS_PRIVATE | \ReflectionProperty::IS_PROTECTED) as $prop) {
+            foreach ($reflect->getProperties(\ReflectionProperty::IS_PRIVATE |
+                \ReflectionProperty::IS_PROTECTED) as $prop) {
                 $prop->setAccessible(true);
                 $value = $prop->getValue($entity);
                 if (! $value instanceof \DateTime || $value->getTimezone()->getName() === 'UTC') {
@@ -223,7 +140,10 @@ class ActivityListener{
                 $value->setTimezone($zone);
                 $prop->setValue($entity, $value);
                 $prop->setAccessible(false);
-                $unitOfWork->recomputeSingleEntityChangeSet($entityManager->getClassMetadata(get_class($entity)), $entity);
+                $unitOfWork->recomputeSingleEntityChangeSet(
+                    $entityManager->getClassMetadata(get_class($entity)),
+                    $entity
+                );
             }
         }
     }
@@ -240,24 +160,21 @@ class ActivityListener{
             return;
         }
         //Get hold of the entities that are scheduled for insertion or update
-            $reflect = new \ReflectionObject($entity);
-            foreach ($reflect->getProperties(\ReflectionProperty::IS_PRIVATE | \ReflectionProperty::IS_PROTECTED) as $prop) {
-                $prop->setAccessible(true);
-                $value = $prop->getValue($entity);
-                if (! $value instanceof \DateTime || $value->getTimezone()->getName() === 'UTC') {
-                    $prop->setAccessible(false);
-                    continue;
-                }
-                //we have wrong time. we must have time in local.
-                $time_from_db = $value->format('Y-m-d H:i:s');
-
-                $realTime = new \DateTime($time_from_db,$Basezone);
-                //$value = $realTime->setTimezone($Zone);
-                //$value->setTimezone($zone);
-                $prop->setValue($entity, $realTime);
+        $reflect = new \ReflectionObject($entity);
+        foreach ($reflect->getProperties(\ReflectionProperty::IS_PRIVATE |
+            \ReflectionProperty::IS_PROTECTED) as $prop) {
+            $prop->setAccessible(true);
+            $value = $prop->getValue($entity);
+            if (! $value instanceof \DateTime || $value->getTimezone()->getName() === 'UTC') {
                 $prop->setAccessible(false);
-                //$unitOfWork->recomputeSingleEntityChangeSet($entityManager->getClassMetadata(get_class($entity)), $entity);
+                continue;
             }
+            //we have wrong time. we must have time in local.
+            $time_from_db = $value->format('Y-m-d H:i:s');
+            $realTime = new \DateTime($time_from_db, $Basezone);
+            $prop->setValue($entity, $realTime);
+            $prop->setAccessible(false);
+        }
 
     }
 
