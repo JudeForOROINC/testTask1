9c88bd15772e39f7337ce3af342c4d2774ad61ff
JudeForOROINC <gkudryashov@magecore.com>
2015-07-15 20:26:27 +0300

    OOT-777: made a unit test fix and code style fix  - refactor. tests fix Forms

--- testTask1/src/Magecore/Bundle/TestTaskBundle/Tests/Unit/EventListener/MailerListenerTest.php
+++ testTask1/src/Magecore/Bundle/TestTaskBundle/Tests/Unit/EventListener/MailerListenerTest.php
@@ -2,6 +2,7 @@
 
 namespace Magecore\Bundle\TestTaskBundle\Tests\Unit\EventListener;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Magecore\Bundle\TestTaskBundle\Entity\Activity;
 use Magecore\Bundle\TestTaskBundle\Entity\Comment;
 use Magecore\Bundle\TestTaskBundle\Entity\DicPriority;
@@ -20,28 +21,175 @@ use Magecore\Bundle\TestTaskBundle\DataFixtures\ORM as ORM;
 class MailerListenerTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testSet()
+    /**
+     *@dataProvider dataProvider
+     */
+    public function testSet($users, $funk, $pattern, $letter)
     {
-        $mock = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
 
-        //$arg = $this->getMock('Doctrine\ORM\Event\LifecycleEventArgs')
-        ;
-
-        $listener = new MailerListener($mock);
-
-        //$listener->pushMail(New Activity());
         $issue = $this->getMock('Magecore\Bundle\TestTaskBundle\Entity\Issue');
+        $usersArray = [];
+        foreach ($users as $u) {
+            $user = $this->getMock('Magecore\Bundle\TestTaskBundle\Entity\User');
 
-        $collaborators = $this->getMock('Doctrine\Common\Collections\Collection ');
+            $user->expects($this->once())->method('getFullName')->will($this->returnValue($u['getFullName']));
+            $user->expects($this->once())->method('getEmail')->will($this->returnValue($u['getEmail']));
+            $usersArray[] = $user;
+        }
+        $collaborators = new ArrayCollection($usersArray);
 
-       // $issue->expects($this->any())->method('getCollaborators')->will($this->returnValue($collaborators));
+        $issue->expects($this->once())->method('getCollaborators')->will($this->returnValue($collaborators));
 
         $activity = $this->getMock('Magecore\Bundle\TestTaskBundle\Entity\Activity');
 
         $activity->expects($this->once())->method('getIssue')->will($this->returnValue($issue));
+        if ($funk !='none') {
+            $activity->expects($this->exactly(count($users)))->method($funk)->will($this->returnValue(true));
+        }
 
+        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
 
-        $listener->formMailAction($activity);
+        $trans = $this->getMock('Symfony\Component\Translation\TranslatorInterface');
+
+        $render = $this->getMock('Symfony\Bundle\FrameworkBundle\Templating\EngineInterface');
+
+        $render->expects($this->exactly(count($users)))->method('render')->will($this->returnValue('ok'));
+
+        if ($funk=='isNewIssueType') {
+            $trans->expects($this->exactly(count($users)))->method('trans')->with(
+                $this->equalTo('CIAT')
+            )->will($this->returnValue('trance ok '));
+        }
+        if ($funk=='isChangeStatusType') {
+            $trans->expects($this->exactly(count($users)))->method('trans')->with(
+                $this->equalTo('CSAT')
+            )->will($this->returnValue('trance ok '));
+        }
+        if ($funk=='isCommentType') {
+            $trans->expects($this->exactly(count($users)))->method('trans')->with(
+                $this->equalTo('COAT')
+            )->will($this->returnValue('trance ok '));
+        }
+
+        foreach ($users as $k => $u) {
+            $render->expects($this->at($k))->method('render')
+                ->with(
+                    $this->equalTo($pattern),
+                    array(
+                        'entity' => $activity, 'name' => $u['getFullName']
+                    )
+                );
+
+        }
+
+        if ($funk != 'none') {
+            foreach ($users as $k => $u) {
+                $container->expects($this->at(1+$k*2))->method('get')->with($this->equalTo('templating'))
+                    ->will($this->returnValue(
+                        $render
+                    ));
+                $container->expects($this->at($k*2))->method('get')
+                    ->with($this->equalTo('translator'))
+                    ->will($this->returnValue(
+                        $trans
+                    ));
+            }
+        } else {
+            $container->expects($this->once())->method('get')->with($this->equalTo('templating'))
+                ->will($this->returnValue(
+                    $render
+                ));
+        }
+
+        $listener = new MailerListener($container);
+
+        $resultArray = $listener->formMailAction($activity);
+        $this->assertEquals($letter, $resultArray);
+
 
     }
+
+
+    /**
+     *
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return array(
+            array(
+                array(array('getEmail'=>'mail1',
+                    'getFullName'=>'testname1'
+                ),),
+                'func' =>'none',
+                'pattern'=>'MagecoreTestTaskBundle:Mailer:view.html.twig',
+                'letter'=>array(
+                    array(
+                        'mail'=>'mail1',
+                        'name'=>'testname1',
+                        'title'=>'Event for testname1',
+                        'letter'=>'ok',
+                    )
+                )
+            ),
+            array(
+                array(
+                    array('getEmail'=>'mail2',
+                        'getFullName'=>'testname2'
+
+                        ),
+                    array('getEmail'=>'mail3',
+                        'getFullName'=>'testname3'
+                        ),
+                    ),
+                'func' =>'isNewIssueType',
+                'pattern'=>'MagecoreTestTaskBundle:Mailer:cnia.html.twig',
+                'letter'=>array(
+                    array(
+                        'mail'=>'mail2',
+                        'name'=>'testname2',
+                        'title'=>'trance ok ',
+                        'letter'=>'ok',
+                    ),
+                    array(
+                        'mail'=>'mail3',
+                        'name'=>'testname3',
+                        'title'=>'trance ok ',
+                        'letter'=>'ok',
+                    )
+                )
+            ),
+            array(
+                array(array('getEmail'=>'mail4',
+                    'getFullName'=>'testname4'
+                )),
+                'func' =>'isChangeStatusType',
+                'pattern'=>'MagecoreTestTaskBundle:Mailer:chia.html.twig',
+                'letter'=>array(
+                    array(
+                        'mail'=>'mail4',
+                        'name'=>'testname4',
+                        'title'=>'trance ok ',
+                        'letter'=>'ok',
+                    )
+                )
+            ),
+            array(
+                array(array('getEmail'=>'mail5',
+                    'getFullName'=>'testname5'
+                )),
+                'func' =>'isCommentType',
+                'pattern'=>'MagecoreTestTaskBundle:Mailer:coia.html.twig',
+                'letter'=>array(
+                    array(
+                        'mail'=>'mail5',
+                        'name'=>'testname5',
+                        'title'=>'trance ok ',
+                        'letter'=>'ok',
+                    )
+                )
+            )
+        );
+    }
+
 }
