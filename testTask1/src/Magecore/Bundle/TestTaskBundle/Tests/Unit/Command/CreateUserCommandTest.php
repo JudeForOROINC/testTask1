6fb23203ae25b61d412733f7dbcdd320069b47c4
JudeForOROINC <gkudryashov@magecore.com>
2015-07-16 20:08:51 +0300

    OOT-777: made a unit test fix and code style fix  - refactor. tests fix commands

--- testTask1/src/Magecore/Bundle/TestTaskBundle/Tests/Unit/Command/CreateUserCommandTest.php
+++ testTask1/src/Magecore/Bundle/TestTaskBundle/Tests/Unit/Command/CreateUserCommandTest.php
@@ -3,27 +3,79 @@
 namespace Magecore\Bundle\TestTaskBundle\Tests\Unit\EventListener;
 
 use Magecore\Bundle\TestTaskBundle\Command\CreateUserCommand;
-use Magecore\Bundle\TestTaskBundle\Entity\Activity;
-use Magecore\Bundle\TestTaskBundle\Entity\Comment;
-use Magecore\Bundle\TestTaskBundle\Entity\DicPriority;
-use Magecore\Bundle\TestTaskBundle\Entity\DicResolution;
-use Magecore\Bundle\TestTaskBundle\Entity\DicStatus;
-use Magecore\Bundle\TestTaskBundle\Entity\Project;
-use Magecore\Bundle\TestTaskBundle\Entity\User;
-use Magecore\Bundle\TestTaskBundle\EventListener\ActivityListener;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\Response;
-use Magecore\Bundle\TestTaskBundle\Entity\Issue;
 use Magecore\Bundle\TestTaskBundle\DataFixtures\ORM as ORM;
-use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
 
 class CreateUserCommandTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testSet()
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testExecute()
     {
-        $command = new CreateUserCommand();
+        $application = new Application();
+        $application->add(new CreateUserCommand());
+
+        $command = $application->find('my:user:create');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        //$this->assertRegExp('/.../', $commandTester->getDisplay());
+
+    }
+
+    public function testExecuteHelp()
+    {
+        $application = new Application();
+
+        $commande = new CreateUserCommand();
+
+
+        $container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
+
+        $doctrine  = $this->getMock('Symfony\Bridge\Doctrine\RegistryInterface');
+
+        $manager = $this->getMock('Doctrine\ORM\EntityManagerInterface');
+
+        $doctrine->expects($this->once())->method('getEntityManager')->will($this->returnValue($manager));
+
+        $container->expects($this->once())->method('get')->with($this->equalTo('doctrine'))->will($this->returnValue($doctrine));
+
+        $QueryBuilder = $this->getMockBuilder('Doctrine\ORM\QueryBuilder')->disableOriginalConstructor()->getMock();
+        $QueryBuilder->expects($this->any())->method('select')->will($this->returnSelf());
+        $QueryBuilder->expects($this->any())->method('from')->will($this->returnSelf());
+        $QueryBuilder->expects($this->any())->method('where')->will($this->returnValue($QueryBuilder));
+        $QueryBuilder->expects($this->any())->method('setParameter')->will($this->returnValue($QueryBuilder));
+
+        $query = $this->getMockBuilder('Doctrine\ORM\AbstractQuery')->disableOriginalConstructor()->getMockForAbstractClass();
+
+        $QueryBuilder->expects($this->any())->method('getQuery')->will($this->returnValue($query));
+
+        $manager->expects($this->once())->method('createQueryBuilder')->will($this->returnValue($QueryBuilder));
+
+        $commande->setContainer($container);
+
+        $application->add($commande);
+
+        $command = $application->find('my:user:create');
+        //$application->set
+        //$command->set
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command' => $command->getName(),
+        'username' => 'username',
+        'email' => 'email',
+        'password' => 'password',
+        'fullname' => 'full',
+            '--help' => true,
+        ));
+
+        //$this->assertRegExp('/.../',
+        var_dump($commandTester->getDisplay());
 
     }
 
